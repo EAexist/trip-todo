46a91b89d48d3f2ada50816c8c2f7d5d499bd8b0
Pyo Hyeon <megatwins@kaist.ac.kr>
2026-08-03 05:22:26 +0900

    fix: add guard component of undefined store objects before calling useStore hooks

--- app/screens/TripList/TripListScreen.tsx
+++ app/screens/TripList/TripListScreen.tsx
@@ -1,11 +1,33 @@
-import { AuthenticatedStackScreenProps, navigate } from '@/navigators'
+import { ActivityIndicator } from '@/components/ActivityIndicator'
+import { useUserStore } from '@/models'
+import { AuthenticatedStackScreenProps, goBack, navigate } from '@/navigators'
 import { HeaderCenterTitle, useHeader } from '@/utils/useHeader'
+import { observer } from 'mobx-react-lite'
 import { FC } from 'react'
+import { LoadingScreen } from '../Loading/LoadingBoundary'
 import { TripListScreenBase } from './TripListScreenBase'
 
 export const TripListScreen: FC<
     AuthenticatedStackScreenProps<'TripList'>
 > = ({}) => {
+    return <TripListGuard />
+}
+
+const TripListGuard = observer(() => {
+    const { activeTrip } = useUserStore()
+
+    if (!activeTrip) {
+        return (
+            <LoadingScreen
+                title={'잠시만 기다려 주세요'}
+                activityIndicator={<ActivityIndicator />}
+                fabTitle={'돌아가기'}
+                onPressFab={() => goBack()}
+                variant={'simple'}
+            />
+        )
+    }
+
     useHeader({
         backgroundColor: 'secondary',
         rightActionTitle: '삭제',
@@ -14,4 +36,4 @@ export const TripListScreen: FC<
     })
 
     return <TripListScreenBase />
-}
+})
